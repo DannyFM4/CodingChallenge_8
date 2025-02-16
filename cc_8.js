ffa7f2b51ff96fccc087b93d6aa6d66af90b19a5
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-16 15:43:48 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -10,3 +10,17 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // lines below ren the function using the parameters provided
 calculateSalary(5000, 500, 0.1);
 calculateSalary(7000, 1000, 0.15);
+
+
+// Task 2: Function Expression
+
+// the lines below create a function to calculate the discount of a purchase
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);  // creates the equation that will be used in the function
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`); // logs the result in the console
+};
+
+
+// the lines below run the function using the parameters provided
+calculateDiscount(100, 0.2);
+calculateDiscount(250, 0.15);
